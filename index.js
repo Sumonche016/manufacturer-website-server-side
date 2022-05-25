b4fdfa8fcbf42cdf07b41042e292a1d8e1896ce5
Sumonche016 <sumonche016@gmail.com>
2022-05-26 04:07:40 +0600

    hii

--- index.js
+++ index.js
@@ -49,6 +49,14 @@ async function run() {
             res.send(result)
         })
 
+        // detete api 
+        app.delete('/myorder/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
     }
 
